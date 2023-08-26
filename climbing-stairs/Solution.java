55a8c2cd5b2d3d18ae3c368255a3d7c9a04aabc5
shubham wankhede <shubhamwankhede@shubhams-MacBook-Air.local>
2023-08-26 13:31:29 +0530

    Problem 9 : Climbing Stairs

---
+++ climbing-stairs/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int climbStairs(int n) {
+        if (n<=2) {
+            return n;
+        }
+        int prev = 1, curr = 2;
+        for (int i = 3; i <= n; i++) {
+            int temp = curr;
+            curr = prev + curr;
+            prev = temp;
+        }
+        return curr;
+    }
+}
